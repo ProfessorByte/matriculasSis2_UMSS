b6c824e232c4f47832b7ac87c31a32ba7ba584a7
GabyC116 <83257181+GabyC116@users.noreply.github.com>
2021-04-26 18:52:02 -0800

    Update MatriculasSis2.java

--- src/matriculassis2/MatriculasSis2.java
+++ src/matriculassis2/MatriculasSis2.java
@@ -3,7 +3,7 @@ package matriculassis2;
 public class MatriculasSis2 {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola GabyChula");
     }
     
 }
